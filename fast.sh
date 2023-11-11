d566e82dd4f700c0726ec0c063c04c205fbb52b6
Mypainismorethanyours <109569168+Mypainismorethanyours@users.noreply.github.com>
2023-11-10 20:21:16 -0500

    delete

--- fast.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-#
-# train for a few epochs while performing "fast sweeps"
-#
-
-python3 ../code/train.py --epochs 5
--- report.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# generate an HTML report of the last training session
-#
-
-jupyter nbconvert --execute ../code/report.ipynb --no-input --to html --output-dir ./
